568ad61ec207b90ff6b428761af5fb9a30f38f02
msaleh1383 <m.smasoudi72@gmail.com>
2019-11-06 06:16:26 +0330

    Add project files.

---
+++ Simple1.py
@@ -0,0 +1,8 @@
+print(3 ** 3) # it means 3 in power of 3
+print (49 ** 0.5) # it means get squre of 49
+print("##############")
+
+print(68 / 6) 
+print(68 % 6) # returns remaining of division
+print("##############")
+print(68 // 6) # returns a integer part of division
